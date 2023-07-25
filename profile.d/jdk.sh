a140845f3f048d4fd3e516f4900f2c0b2652b102
kevin <kevin197011.l@gmail.com>
2023-07-26 07:54:08 +0800

    Update 2023-07-26 07:54:08.

---
+++ profile.d/jdk.sh
@@ -0,0 +1,8 @@
+# Copyright (c) 2023 kk
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+export JAVA_HOME=/usr/local/jdk
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
